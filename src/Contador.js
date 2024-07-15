b31caddd8dc7b2a360b31e2bdc252eb1d42cdfd1
Arthur Cechinel Nunes <an6281745@gmail.com>
2024-07-15 11:31:14 -0300

    Initial commit

---
+++ src/Contador.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const Contador = () => {
+  const [contador, setContador] = useState(0);
+
+  const aumentar = () => {
+    setContador(contador + 1);
+  };
+
+  const diminuir = () => {
+    setContador(contador - 1);
+  };
+
+  return (
+    <div>
+      <h1>Contador: {contador}</h1>
+      <button onClick={aumentar}>Aumentar</button>
+      <button onClick={diminuir}>Diminuir</button>
+    </div>
+  );
+};
+
+export default Contador;
